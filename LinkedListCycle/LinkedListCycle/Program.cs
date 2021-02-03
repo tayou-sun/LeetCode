e7d3fe0f3765fa2eaa3d8b436cade6badad0b82f
tayou-sun <tayou.dun@gmail.com>
2021-02-03 19:48:09 +0300

    cycle task

--- LinkedListCycle/LinkedListCycle/Program.cs
+++ LinkedListCycle/LinkedListCycle/Program.cs
@@ -2,6 +2,41 @@
 
 namespace LinkedListCycle
 {
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+                return false;
+
+            var slow = head;
+            var fast = head.next;
+
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null)
+                    return false;
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
