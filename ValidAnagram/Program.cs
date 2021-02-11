2cfe175e202811d080c3f3d6b3b5b506eafa66b5
tayou.sun <tayou.sun@gmail.com>
2021-02-11 21:49:30 +0300

    Valid Anagram

---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidAnagram
+{
+    public class Solution {
+    public bool IsAnagram(string s, string t) {
+        var aArr = s.ToCharArray();
+        var tArr = t.ToCharArray();
+
+        Array.Sort(aArr);
+        Array.Sort(tArr);
+
+       /*  foreach(var a in aArr){
+            Console.WriteLine(a);
+        }
+Console.WriteLine("________________");
+        foreach(var ta in tArr){
+            Console.WriteLine(ta);
+        } */
+
+        return Enumerable.SequenceEqual(tArr, aArr);
+
+    }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s  = new Solution();
+            var res = s.IsAnagram("ana1gram", "nagaram");
+            Console.WriteLine(res);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
