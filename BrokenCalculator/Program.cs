9cf7600a4576769971ae709c48001cd07e97c6fc
tayou.sun <tayou.sun@gmail.com>
2021-02-26 22:40:28 +0300

    first

---
+++ BrokenCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BrokenCalculator
+{
+    public class Solution
+    {
+        public int BrokenCalc(int X, int Y)
+        {
+            var res = 0;
+
+            while (Y > X)
+            {
+                res++;
+                if (Y % 2 == 1)
+                {
+                    Y++;
+                }
+                else
+                {
+                    Y = Y / 2;
+                }
+            }
+
+            return res + X - Y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var res = s.BrokenCalc(1, 1000000000);
+
+            Console.WriteLine(res);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FindLongestWord/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindLongestWord
+{
+    public class Solution
+    {
+        public string FindLongestWord(string s, IList<string> d)
+        {
+
+            var res = "";
+            var ss = String.Concat(s.OrderBy(c => c).Distinct());
+            foreach (var elem in d)
+            {
+                var a = String.Concat(elem.OrderBy(c => c).Distinct());
+                if (ss.Contains(a) && a.Length > res.Length)
+                {
+                    res = a;
+                }
+
+
+            }
+
+
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var res = s.FindLongestWord("abpcplea", new List<string>() { "ale", "apple", "monkey", "plea" });
+            Console.WriteLine(res);
+        }
+    }
+}
